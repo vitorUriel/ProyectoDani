1d642109fb94f9ed2cc95012645562461573c520
vitorUriel <2336000045@utna.edu.mx>
2026-03-26 22:28:07 -0600

    Agregando archivos

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const router = require('./routes')
+const path = require('path');
+
+// Configurar Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', { titulo: 'Mi Proyecto Express' });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+});
